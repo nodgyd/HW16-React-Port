c806852bffc9c89b60321e638833521a0cb97c9a
Naji Dia <najimdia@gmail.com>
2022-02-27 15:22:04 -0800

    almost done

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+export default function Footer( {resumeData: resumeData} ) {
+    return (
+      <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {
+              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
+                return(
+                  <li>
+                    <a target="_blank" href={item.url}>
+                    <i target="_blank" className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
+    );
+}
---
+++ src/resumeData.js
@@ -0,0 +1,93 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/nodgyd",
+    "name": "Naji Maan Dia",
+    "role": "Full Stack Developer",
+    "roleDescription": "I love anything and everything to do with technology. My thirst to learn in this industry is what pushes me to learn how to create bigger and better things!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/najidia/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nodgyd",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently working a job at a FinTech company as a Corporate Bussiness Analyst, looking to dive into a more technical role to expand and broaden my skills!",
+    "address":"US",
+    "website":"https://github.com/nodgyd",
+    "education":[
+      {
+        "UniversityName":"University of California, Riverside",
+        "specialization":"B.S., Business Administration",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"UCI Continued Education",
+        "specialization":"The Coding Bootcamp Certificate",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2022",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"New American Funding",
+        "specialization":"Corporate Business Analyst",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"-",
+        "Achievements":"NAF 360 Champion"
+      },
+      {
+        "CompanyName":"New American Funding",
+        "specialization":"Assistant Project Manager",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2021"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"tech blog",
+        "imgurl":"images/portfolio/coffee.jpg",
+        "gitURL": "https://github.com/nodgyd/HW12-Tech-Blog"
+      },
+      {
+        "name":"project2",
+        "description":"grubnub",
+        "imgurl":"images/portfolio/console.jpg",
+        "gitURL": "https://github.com/nodgyd/GrubNub"
+      },
+      {
+        "name":"project3",
+        "description":"crypto champs",  
+        "imgurl":"images/portfolio/project2.png",
+        "gitURL": "https://github.com/nodgyd/CryptoChamps"
+      },
+      {
+        "name":"project4",
+        "description":"team profile generator",
+        "imgurl":"images/portfolio/phone.jpg",
+        "gitURL": "https://github.com/nodgyd/HW8-Team-Profile-Generator"
+      }
+    ]
+  }
+  
+  export default resumeData
